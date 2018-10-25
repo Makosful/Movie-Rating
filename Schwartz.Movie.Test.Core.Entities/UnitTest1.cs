3a906ea1ee4532ff9c355a927d79844ad0d538be
Axl <makosful@gmail.com>
2018-10-25 09:39:56 +0200

    Added some test classes and the MOQ nuget for testing

---
+++ Schwartz.Movie.Test.Core.Entities/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Schwartz.Movie.Test.Core.Entities
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
