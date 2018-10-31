39d2f42bdbf5866b9c69a723dbac3df4b0c35fcf
Axl <makosful@gmail.com>
2018-10-31 14:31:58 +0100

    Added another test case to ensure uniformaty. Not yet implemented.

--- Schwartz.Movie.Test.Infrastructure.Data/ReviewRepositoryTest.cs
+++ Schwartz.Movie.Test.Infrastructure.Data/ReviewRepositoryTest.cs
@@ -12,10 +12,24 @@ namespace Schwartz.Movie.Test.Infrastructure.Data
         [InlineData(-10)]
         public void GetReviewsByReviewer_InvalidReviewerId_ExpectsEmptyList(int id)
         {
-            IReviewRepository reviewRepository = new ReviewRepository();
-            var list = reviewRepository.GetReviewsByReviewer(id);
+            IReviewRepository repository = new ReviewRepository();
+            var ratings = repository.GetReviewsByReviewer(id);
 
-            Assert.NotNull(list);
+            Assert.NotNull(ratings);
+            Assert.Empty(ratings);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        [InlineData(-10)]
+        public void GetReviewsByMovie_InvalidMovieId_ExpectsEmptyLists(int id)
+        {
+            IReviewRepository repository = new ReviewRepository();
+            var ratings = repository.GetReviewsByMovie(id);
+
+            Assert.NotNull(ratings);
+            Assert.Empty(ratings);
         }
     }
 }
